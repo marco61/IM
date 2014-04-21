c663cd0fdf9d29f771939cbe94b58efea1704c4b
marco61 <marco61@iastate.edu>
2014-04-20 21:46:33 -0500

    fixes

--- src/com/team/innovation/MainActivity.java
+++ src/com/team/innovation/MainActivity.java
@@ -13,12 +13,6 @@ public class MainActivity extends AndroidApplication {
         cfg.useGL20 = true;
         cfg.useAccelerometer = false;
         cfg.useCompass = false;
-        int checkH = 0;
-        TIM tim = new TIM();
-        MainMenuScreen mms = new MainMenuScreen(tim);
-        
-       //initialize(tim, cfg);
-       //checkH = mms.check;
         
        initialize(new Main_Game_Class(), cfg);
    
